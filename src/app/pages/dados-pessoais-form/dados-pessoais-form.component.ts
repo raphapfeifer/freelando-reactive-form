f064a630b323cf9394ce99eaaf331ddf9bd92fe4
unknown <raphael.alcateia>
2026-01-12 16:29:16 -0300

    User registration

--- src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
+++ src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponent } from '../../shared/components/button/button.component';
+import { CadastroService } from '../../shared/services/cadastro.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dados-pessoais-form',
@@ -14,10 +16,53 @@ import { ButtonComponent } from '../../shared/components/button/button.component
   templateUrl: './dados-pessoais-form.component.html',
   styleUrls: ['./dados-pessoais-form.component.scss']
 })
-export class DadosPessoaisFormComponent {
+export class DadosPessoaisFormComponent implements OnInit{
   dadosPessoaisForm!: FormGroup;
 
-  onAnterior(): void {}
+  estados = [
+    {sigla: 'AC', nome: 'Acre'}
+  ]
 
-  onProximo(): void {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private cadastroService: CadastroService
+  ){}
+
+  ngOnInit(): void {
+    this.dadosPessoaisForm = this.fb.group({
+      nomeCompleto: ['', Validators.required],
+      estado: ['', Validators.required],
+      cidade: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmaSenha: ['', Validators.required]
+    })
+  }
+
+  onAnterior(): void {
+    this.salvarDadosAtuais();
+    this.router.navigate(['/cadastro/area-atuacao']);
+  }
+
+  onProximo(): void {
+    if(this.dadosPessoaisForm.valid){
+      this.salvarDadosAtuais();
+      this.router.navigate(['/cadastro/confirmacao']);
+    }else{
+      this.dadosPessoaisForm.markAllAsTouched();
+    }
+  }
+
+  private salvarDadosAtuais(){
+    const formValue = this.dadosPessoaisForm.value;
+
+    this.cadastroService.updateCadastroData({
+      nomeCompleto: formValue.nomeCompleto,
+      estado: formValue.estado,
+      cidade: formValue.cidade,
+      email: formValue.email,
+      senha: formValue.senha
+    })
+  }
 }
